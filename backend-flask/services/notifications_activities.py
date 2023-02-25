30dfa27c8228f0de38fb7c942313f82568614913
Ibrahim Olanigan <salalite@gmail.com>
2023-02-25 15:36:00 +0000

    Add notification endpoint and frontend page

---
+++ backend-flask/services/notifications_activities.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta, timezone
+class NotificationsActivities:
+  def run():
+    console.log("Enter Notif Activity")
+    now = datetime.now(timezone.utc).astimezone()
+    console.log("Current Date", now)
+    results = [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'Benjamin Yahya',
+      'message': 'Here we go!!!',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'likes_count': 5,
+      'replies_count': 1,
+      'reposts_count': 0,
+      'replies': [{
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'Aina Ola',
+        'message': 'Woww!',
+        'likes_count': 6,
+        'replies_count': 4,
+        'reposts_count': 2,
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }],
+    },
+    {
+      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
+      'handle':  'Ahmad Wolfgang',
+      'message': 'I heard a thing',
+      'created_at': (now - timedelta(days=7)).isoformat(),
+      'expires_at': (now + timedelta(days=9)).isoformat(),
+      'likes': 2,
+      'replies': []
+    }
+    ]
+    return results
